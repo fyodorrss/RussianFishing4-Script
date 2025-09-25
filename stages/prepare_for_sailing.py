48aa7e823bd9b54a8a5280e32af76debc84fef96
mono976162467@gmail.com <13544102930@163.com>
2025-09-26 00:22:40 +0800

    fix

--- stages/prepare_for_sailing.py
+++ stages/prepare_for_sailing.py
@@ -9,66 +9,6 @@ def prepare_for_sailing():
     准备出海
     """
 
-    # 拿出鱼竿
-    if config.is_fly_ticket:
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.key_down('U')
-        sleep_time(random.uniform(0.42, 0.52))
-        cout = 0
-        max_attempts = 10  # 最大尝试次数
-        flyrod_regions=[]
-        while not config.stop_event.is_set() and cout < max_attempts:
-            cout += 1
-            flyrod_regions=utils.find_template_in_regions(config.FlyRodRegionScreenshot, 'flyrod.png', confidence=0.95)
-            if len(flyrod_regions) > 0:
-                drr=flyrod_regions[0]
-                utils.move_mouse_random_in_region((drr["left"], drr["top"], drr["width"], drr["height"]))
-                break
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse(0.1)
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.key_up('U')
-        
-        #配置鱼杆
-        sleep_time(random.uniform(2, 2.1))
-        utils.press_key('v')
-        #修改钓组
-        sleep_time(random.uniform(1.42, 1.52))
-        utils.move_mouse_random_in_region((1105,258,80,17))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.move_mouse_random_in_region((1039,421,159,47))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(0.1)
-        utils.click_left_mouse()
-        #选择钩子
-        sleep_time(random.uniform(1.42, 1.52))
-        utils.move_mouse_random_in_region((1001,603,64,64))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.move_mouse_random_in_region((285, 203, 166, 200))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(0.1)
-        utils.click_left_mouse()
-        #选择饵
-        sleep_time(random.uniform(1.42, 1.52))
-        utils.move_mouse_random_in_region((1001,695,64,64))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.move_mouse_random_in_region((285, 203, 166, 200))
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.click_left_mouse()
-        sleep_time(0.1)
-        utils.click_left_mouse()
-        #关闭界面
-        sleep_time(random.uniform(0.42, 0.52))
-        utils.press_key('v', 0.1)  # 关闭界面
-    
     if not config.stop_event.is_set():
         #向左转
         sleep_time(random.uniform(0.52, 0.65))
